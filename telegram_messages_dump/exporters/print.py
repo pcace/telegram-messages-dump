a0fd2aca9a6866075f63254eb4adedf3aa7fcc1b
Johannes Frölich <mail@johannes-froelich.de>
2022-11-20 18:24:30 +0100

    init print exporter
    feat: :sparkles: init wip
    
    init
    
    init
    
    asdf
    
    asdf
    
    asdf
    
    asdf

---
+++ telegram_messages_dump/exporters/print.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+
+import re
+from .common import common
+
+class print(object):
+    """ directly print messages.
+        By convention it has to be called exactly the same as its file name.
+        (Apart from .py extention)
+    """
+
+    def __init__(self):
+        """ constructor """
+        self.ESCAPE = re.compile(r'[\x00-\x1f\b\f\n\r\t]')
+        self.ESCAPE_DICT = {
+            '\\': '\\\\',
+            # '"': '\\"',
+            '\b': '\\b',
+            '\f': '\\f',
+            '\n': '\\n',
+            '\r': '\\r',
+            '\t': '\\t',
+        }
+        for i in range(0x20):
+            self.ESCAPE_DICT.setdefault(chr(i), '\\u{0:04x}'.format(i))
+
+    def format(self, msg, exporter_context):
+        """ Formatter method. Takes raw msg and converts it to a *one-line* string.
+            :param msg: Raw message object :class:`telethon.tl.types.Message` and derivatives.
+                        https://core.telegram.org/type/Message
+
+            :returns: *one-line* string containing one message data.
+        """
+        # pylint: disable=unused-argument
+        name, _, content, re_id, _, _, _ = common.extract_message_data(msg)
+        # Format a message log record
+        message = "[media]"
+        if self._py_encode_basestring(content) : message = self._py_encode_basestring(content) 
+       
+        msg_dump_str = '[{}-{:02d}-{:02d} {:02d}:{:02d}] {}: {}'.format(
+            msg.date.year, msg.date.month, msg.date.day,
+            msg.date.hour, msg.date.minute,
+            name, message)
+
+        return msg_dump_str
+
+    def begin_final_file(self, resulting_file, exporter_context):
+        """ Hook executes at the beginning of writing a resulting file.
+            (After BOM is written in case of --addbom)
+        """
+        pass
+
+    # This code is inspired by Python's json encoder's code
+    def _py_encode_basestring(self, s):
+        """Return a JSON representation of a Python string"""
+        if not s:
+            return s
+        def replace(match):
+            return self.ESCAPE_DICT[match.group(0)]
+        return self.ESCAPE.sub(replace, s)
